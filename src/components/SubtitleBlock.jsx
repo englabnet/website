e8fae89704029b90709263bb658a4e9dd87021c0
Nikita Kuchur <nikitakuchur@gmail.com>
2023-09-24 23:42:34 +0200

    fixed changing size

--- src/components/SubtitleBlock.jsx
+++ src/components/SubtitleBlock.jsx
@@ -20,18 +20,23 @@ function SubtitleBlock({ subtitles, currentTime }) {
         low = mid + 1;
       }
     }
-    return null; // Not found
+    // We found nothing, so let's pick the closest segment.
+    const segment = segments[high];
+    return { prev: segments[high - 1], current: segment, next: segments[high + 1] };
   };
 
   const segment = binarySearchByTime(currentTime, subtitles);
+
+  const currentSegmentColor = colorScheme === 'dark' ? 'blue.3' : 'blue.6';
+  const closeSegmentColor = colorScheme === 'dark' ? 'blue.8' : 'blue.3';
 
   return (
     <Stack align="center" style={{ gap: '10px' }}>
       {segment ? (
         <>
-          <Text size="xl" color={colorScheme === 'dark' ? 'blue.8' : 'blue.3'}>{segment.prev ? segment.prev.text : null}</Text>
-          <Text size="xl" fw={500} color={colorScheme === 'dark' ? 'blue.3' : 'blue.6'}>{segment.current.text}</Text>
-          <Text size="xl" color={colorScheme === 'dark' ? 'blue.8' : 'blue.3'}>{segment.next ? segment.next.text : null}</Text>
+          <Text size="xl" mih={31} color={closeSegmentColor}>{segment.prev ? segment.prev.text : null}</Text>
+          <Text size="xl" mih={31} fw={500} color={currentSegmentColor}>{segment.current.text}</Text>
+          <Text size="xl" mih={31} color={closeSegmentColor}>{segment.next ? segment.next.text : null}</Text>
         </>
       ) : null}
     </Stack>
