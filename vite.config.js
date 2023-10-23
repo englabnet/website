99895e0b47678771255cc7f294e5f93ac8c29c0e
Nikita Kuchur <nikitakuchur@gmail.com>
2023-10-24 00:32:57 +0200

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': env.BACKEND_URL,
-      }
+export default defineConfig({
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8080'
     },
-    plugins: [react()],
-  }
+  },
+  plugins: [react()],
 })
